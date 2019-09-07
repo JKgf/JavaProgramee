1fcf34d4b3e21c466df64c296efa4456772e9ba0
Jkgf <3439855622@qq.com>
2019-09-07 17:12:09 +0800

    the first work

--- TheFirstWeek/src/com/test/Test.java
+++ TheFirstWeek/src/com/test/Test.java
@@ -1,8 +1,9 @@
 package com.test;
-
+// 动态画出各种几何图形
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -20,7 +21,6 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 
 	private static Color c;
 
-
 	private int flag = 5;
 
 	private static final long serialVersionUID = 1L;
@@ -29,80 +29,89 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 		this.setSize(1920, 1080);
 		this.setVisible(true);
 		this.setLayout(null);
+		this.setFont(new Font("宋体", Font.PLAIN, 20));
 		this.setTitle("java绘图程序");
+		setLocationRelativeTo(null);// 把窗口位置设置到屏幕中心
 		this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		this.setResizable(true);// 不能改变窗体大小
-		this.setBackground(Color.WHITE);
+		this.setBackground(Color.white);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.addMouseListener(this);
-		this.getContentPane().setBackground(Color.WHITE);
+		this.getContentPane().setBackground(Color.white);
 
-		Button button1, button2, button3, button4, 
-			   button5, button6, button7, button8, 
-			   button9,button10, button11, button12;
-		button1 = new Button("红色");
-		button1.setBounds(0, 0, 80, 30);
-		button1.setBackground(Color.red);
-		button1.addActionListener(this);
-		this.add(button1);
+		Button yellowButton, greenButton, redButton, orangeButton, 
+			   eraserButton, cancleButton, deleteAllButton, circleButton, 
+			   rectangleButton,roundedRectangleButton, arcButton, 
+			   rectangle3DButton, textButton;
+		yellowButton = new Button("黄色");
+		yellowButton.setBounds(0, 0, 140, 40);
+		yellowButton.setBackground(Color.yellow);
+		yellowButton.addActionListener(this);
+		this.add(yellowButton);
 
-		button2 = new Button("绿色");
-		button2.setBounds(90, 0, 80, 30);
-		button2.setBackground(Color.green);
-		button2.addActionListener(this);
-		this.add(button2);
+		greenButton = new Button("绿色");
+		greenButton.setBounds(160, 0, 140, 40);
+		greenButton.setBackground(Color.green);
+		greenButton.addActionListener(this);
+		this.add(greenButton);
 
-		button3 = new Button("黄色");
-		button3.setBounds(180, 0, 80, 30);
-		button3.setBackground(Color.yellow);
-		button3.addActionListener(this);
-		this.add(button3);
+		redButton = new Button("红色");
+		redButton.setBounds(320, 0, 140, 40);
+		redButton.setBackground(Color.red);
+		redButton.addActionListener(this);
+		this.add(redButton);
 
-		button4 = new Button("橙色");
-		button4.setBackground(Color.orange);
-		button4.setBounds(270, 0, 80, 30);
-		button4.addActionListener(this);
-		this.add(button4);
+		orangeButton = new Button("橙色");
+		orangeButton.setBackground(Color.orange);
+		orangeButton.setBounds(480, 0, 140, 40);
+		orangeButton.addActionListener(this);
+		this.add(orangeButton);
 
-		button5 = new Button("橡皮擦");
-		button5.setBounds(360, 0, 80, 30);
-		button5.addActionListener(this);
-		this.add(button5);
+		eraserButton = new Button("橡皮擦");
+		eraserButton.setBounds(640, 0, 140, 40);
+		eraserButton.addActionListener(this);
+		this.add(eraserButton);
 
-		button6 = new Button("撤销");
-		button6.setBounds(450, 0, 80, 30);
-		button6.addActionListener(this);
-		this.add(button6);
+		cancleButton = new Button("撤销");
+		cancleButton.setBounds(800, 0, 140, 40);
+		cancleButton.addActionListener(this);
+		this.add(cancleButton);
 
-		button7 = new Button("全部删除");
-		button7.setBounds(540, 0, 80, 30);
-		button7.addActionListener(this);
-		this.add(button7);
+		deleteAllButton = new Button("全部删除");
+		deleteAllButton.setBounds(960, 0, 140, 40);
+		deleteAllButton.addActionListener(this);
+		this.add(deleteAllButton);
 
-		button8 = new Button("圆形");
-		button8.setBounds(630, 0, 80, 30);
-		button8.addActionListener(this);
-		this.add(button8);
+		circleButton = new Button("圆形");
+		circleButton.setBounds(1120, 0, 140, 40);
+		circleButton.addActionListener(this);
+		this.add(circleButton);
 
-		button9 = new Button("矩形");
-		button9.setBounds(720, 0, 80, 30);
-		button9.addActionListener(this);
-		this.add(button9);
+		rectangleButton = new Button("矩形");
+		rectangleButton.setBounds(1280, 0, 140, 40);
+		rectangleButton.addActionListener(this);
+		this.add(rectangleButton);
 		
-		button10 = new Button("圆角矩形");
-		button10.setBounds(810, 0, 80, 30);
-		button10.addActionListener(this);
-		this.add(button10);
+		roundedRectangleButton = new Button("圆角矩形");
+		roundedRectangleButton.setBounds(1440, 0, 140, 40);
+		roundedRectangleButton.addActionListener(this);
+		this.add(roundedRectangleButton);
 		
-		button11 = new Button("圆弧");
-		button11.setBounds(900, 0, 80, 30);
-		button11.addActionListener(this);
-		this.add(button11);
+		arcButton = new Button("圆弧");
+		arcButton.setBounds(1600, 0, 140, 40);
+		arcButton.addActionListener(this);
+		this.add(arcButton);
 		
-		button12 = new Button("3D矩形");
-		button12.setBounds(990, 0, 80, 30);
-		button12.addActionListener(this);
-		this.add(button12);
+		rectangle3DButton = new Button("3D矩形");
+		rectangle3DButton.setBounds(1760, 0, 140, 40);
+		rectangle3DButton.addActionListener(this);
+		this.add(rectangle3DButton);
+		
+		textButton = new Button("3D矩形");
+		textButton.setBounds(1760, 0, 140, 40);
+		textButton.addActionListener(this);
+		this.add(textButton);
+
 
 	}
 
@@ -114,32 +123,33 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 
 	
 	public void paint(Graphics g) {
+		// super.paint(g); // 可以缩放窗口 
+		g.setColor(Color.white);// 设置画板初始颜色为白色
 		if (c == null)
 			c = g.getColor();
 		g.setColor(c);
 		if (flag == 1) {
 			g.fillOval(x, y, w, h);
-			
 		} else if(flag == 2) {
 			g.fillRect(x, y, w, h);
-		}
-		else if(flag == 3){
-			g.fillRoundRect(x, y, w, h, 20, 20);
+		}else if(flag == 3){
+			g.fillRoundRect(x, y, w, h, 30, 30);
 		}else if(flag == 4){
 			g.fillArc(x, y, w, h, 20, 20);
-		}else {
+		}else if(flag == 5){
 			g.fill3DRect(x, y, w, h, true);
+		}else {
 			
-		}
-		
-
+		}		
 	}
 
+	
 	public void clear(Graphics g) {
-		g.setColor(Color.WHITE);
+		g.setColor(Color.white);
 		g.clearRect(0, 0, 1920, 1080);
 	}
 
+	
 	/**
 	 * 单击
 	 */
@@ -148,6 +158,7 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 
 	}
 
+	
 	/**
 	 * 按下
 	 */
@@ -158,6 +169,7 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 		y = e.getY();
 
 	}
+	
 
 	/**
 	 * 松开
@@ -178,6 +190,7 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 		}
 		paint(getGraphics());
 	}
+	
 
 	/**
 	 * 鼠标进入事件
@@ -186,6 +199,7 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 	public void mouseEntered(MouseEvent e) {
 	}
 
+	
 	/**
 	 * 鼠标移除事件
 	 */
@@ -193,21 +207,21 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 	public void mouseExited(MouseEvent e) {
 	}
 
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
 		switch (e.getActionCommand().hashCode()) {
 		case 1038352:
 			// 红色
-			c = Color.RED;
+			c = Color.red;
 			break;
 		case 1041235:
 			// 绿色
-			c = Color.GREEN;
+			c = Color.green;
 			break;
 		case 1293358:
 			// 黄色
-			c = Color.YELLOW;
+			c = Color.yellow;
 			break;
 		case 877369:
 			// 橙色
@@ -215,11 +229,11 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 			break;
 		case 27138585:
 			// 橡皮擦
-			c = Color.WHITE;
+			c = Color.white;
 			break;
 		case 836828:
 			Graphics graphics = getGraphics();
-			graphics.setColor(Color.WHITE);
+			graphics.setColor(Color.white);
 
 			if (flag == 1) {
 				graphics.fillOval(x, y, w, h);
@@ -262,5 +276,4 @@ public class Test extends JFrame implements MouseListener, ActionListener {
 			break;
 		}
 	}
-
 }
