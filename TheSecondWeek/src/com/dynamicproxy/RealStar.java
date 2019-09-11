4b607f9700e68d7e5260902cbd89536379d81f11
Jkgf <3439855622@qq.com>
2019-09-11 21:01:28 +0800

    proxy

---
+++ TheSecondWeek/src/com/dynamicproxy/RealStar.java
@@ -0,0 +1,36 @@
+package com.dynamicproxy;
+
+public class RealStar implements Star{
+
+	@Override
+	public void confer() {
+		// TODO Auto-generated method stub
+		System.out.println("RealStar.confer()");
+	}
+
+	@Override
+	public void signContract() {
+		// TODO Auto-generated method stub
+		System.out.println("RealStar.signContract()");
+		
+	}
+
+	@Override
+	public void bookTicket() {
+		// TODO Auto-generated method stub
+		System.out.println("RealStar.bookTicket()");
+	}
+
+	@Override
+	public void sing() {
+		// TODO Auto-generated method stub
+		System.out.println("RealStar(Jay Chou).sign()");
+	}
+
+	@Override
+	public void collect() {
+		// TODO Auto-generated method stub
+		System.out.println("RealStar.collect()");
+	}
+
+}
